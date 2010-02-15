141ea9f1794175768b7dcbf623193b9129f542e8
Aad Versteden <madnificent@gmail.com>
2010-02-15 19:43:03 +0100

    Simple accessors to make the editing of json objects a bit easier

---
+++ accessors.lisp
@@ -0,0 +1,48 @@
+(in-package :jsown)
+
+;;;; This file contains the helpers which make it easier to edit the parsed json objects
+
+(defun keywords (object)
+  "Returns a list of all the keywords contained in the object"
+  (mapcar #'car object))
+
+(defun key-val (object key)
+  "Returns the list which represents the key-val pair in the json object"
+  (loop for k-v in object
+     when (string= (car k-v) key)
+     do (return-from key-val k-v))
+  (error "Key ~A is not available in the given object" key))
+
+(defun val (object key)
+  "Returns the value of the given key in object"
+  (cdr (key-val object key)))
+
+(defun push-key (object key value)
+  "Adds the given key to the object"
+  (cons (cons key value) object))
+
+(defun append-key (object key value)
+  "Appends the given key to the object"
+  (setf (cdr (last object))
+	(list (cons key value)))
+  object)
+
+(defun overwrite-val (object key value)
+  "Overwrites the given key's value with value.  Errors out if the key didn't exist"
+  (setf (cdr (key-val object key)) value)
+  object)
+
+(defun (setf val) (value object key)
+  "Sets the value of the keyword key of the object object to value.  If the key didn't have any value yet, the keyword is added to the object"
+  (handler-case
+      (overwrite-val object key value)
+    (error ()
+      (push-key object key value))))
+
+(defmacro do-json-keys ((key val) object &body body)
+  "Iterates over the json key-value pairs"
+  (let ((k-v (gensym)))
+    `(loop for ,k-v in ,object
+	for ,key = (car ,k-v)
+	for ,val = (cdr ,k-v)
+	do (progn ,@body))))
--- packages.lisp
+++ packages.lisp
@@ -1,6 +1,12 @@
 (defpackage :jsown
   (:use :common-lisp)
+  ;; reading
   (:export :parse
 	   :build-key-container
-	   :parse-with-container
-	   :to-json))
+	   :parse-with-container)
+  ;; writing
+  (:export :to-json)
+  ;; editing
+  (:export :keywords
+	   :val
+	   :do-json-keys)))
